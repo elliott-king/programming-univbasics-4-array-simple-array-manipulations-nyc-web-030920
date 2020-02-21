a3533665d1e595ebeb69c28aed907da3f0082eac
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-21 16:59:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,35 @@ end
 def pop_with_args arr
   arr.pop 2
 end
+
+def using_shift arr
+  arr.shift
+end
+
+def shift_with_args arr
+  arr.shift 2
+end
+
+def using_concat arr, arr2
+  arr.concat(arr2)
+end
+
+def using_insert arr, a
+  arr.insert(3, a)
+end
+
+def using_uniq arr
+  arr.uniq
+end
+
+def using_flatten arr
+  arr.flatten
+end
+
+def using_delete arr, s
+  arr.delete(s)
+end
+
+def using_delete_at arr, i
+  arr.delete_at(i)
+end
